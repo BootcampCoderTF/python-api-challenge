67b7923b19153dc2e1b59fd25df82d4efe5153a7
BootcampCoderTF <tmfararira@gmail.com>
2023-11-27 01:01:00 +0000

    Part 2: VacationPy Completed! Challenge Complete!

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8d2297c489c0c003dbaca6c99744cd78"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c34b64c9c0d04394bfd884bd2b0112c2"
